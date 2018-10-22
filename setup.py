68fe78b989ce6e40ff6abd806a67e79ca034d2ee
Mike Burr <mburr@unintuitive.org>
2018-10-22 15:38:57 -0700

    pkging

--- setup.py
+++ setup.py
@@ -4,11 +4,22 @@
 Even for PoC, this makes things easier. `pip install -e foo`, `./setup.py test`, etc.
 """
 
+import sys
+
+CURRENT_PYTHON = sys.version_info[:2]
+REQUIRED_PYTHON = (3, 5)
+
+if CURRENT_PYTHON < REQUIRED_PYTHON:
+    print('Python >= %s.%s required' % REQUIRED_PYTHON)
+    sys.exit(1)
+
 from distutils.core import setup
 
 name = 'person_search'
 
 setup(
+    python_requires='>={}.{}'.format(*REQUIRED_PYTHON),
+    install_requires=['django>=2.1', 'psycopg2', 'requests', 'psycopg2-binary'],
     name=name,
     version='0.0.1-a',
     provides=[name],
